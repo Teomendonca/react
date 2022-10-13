a8910d3d6d662b069a98dc867310aa0d86204d45
teo.mendonca1510@gmail.com <teo.mendonca1510@gmail.com>
2022-10-12 21:52:09 -0300

    adaptacao benergy

--- src/index.js
+++ src/index.js
@@ -1,65 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+// import './script.js';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <>
+        <nav id='HeaderOptions'>
+          {/* <i>icone</i>
+          <i>icone</i>
+          <i>icone</i> */}
+        </nav>
+        <nav id='HeaderTitle'>BOM B'Energy</nav>
+      </>
+    )
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+}
+class Cadastro extends React.Component {
+  render() {
+    return (
+      <div className='Cadastro'>
+        <fieldset>
+          <legend>Cadastro</legend>
+          <label>Nome do Material:</label>
+          <input type="text" id="nomeAstro" />
+
+          <label>Descrição:</label>
+          <input type="text" id="nomeSistema" />
+
+          <label>Quantidade:</label>
+          <input type="number" id="latitude" />
+
+          <label>Data de compra:</label>
+          <input type="date" id="longitude" />
+          <label>Massa:</label>
+          <input type="number" id='magnetude' />
+          <label>Custo:</label>
+          <input type="number" id='magnetude' />
+          <label>Unidade de medida:</label>
+          <input type="text" id='magnetude' />
+
+          <button id="cadastrar">Cadastrar</button>
+          <button id="limpar">Limpar Formulario</button>
+
+        </fieldset>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Nome do Material</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Data de compra</th>
+              <th>Massa</th>
+              <th>Custo</th>
+              <th>Unidade de medida</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    )
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
-  
+}
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("Cadastro"));
+root.render(<Cadastro />);
+const navbar = ReactDOM.createRoot(document.getElementById("navbar"));
+navbar.render(<NavBar />);
